3af71ccf16399e8bbe4b17b4f9209f826f56a231
Vetik Tsvetik <vetik32@yahoo.com>
2013-05-06 14:56:22 +0300

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('d3DemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
